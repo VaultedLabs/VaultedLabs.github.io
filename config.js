d903679cdb646e6038b9963392173b0148155d46
VaultedLabs <vaultedwealthkey@gmail.com>
2026-09-14 12:34:01 -0400

    Drop coin list from config

--- config.js
+++ config.js
@@ -4,12 +4,5 @@ window.VAULT = {
   x: "https://x.com/VaultStationHQ",
   pump: "https://pump.fun/join/VaultedLabs",
   pumpCode: "VaultedLabs",
-  wallet: "CQknLDqYnrpTGaXf7uSeDEAox8b7zuwJrYDgpryKqm2u",
-  assets: [
-    {
-      mint: "7iAAaqvZx9tihTxsn6CixqyRNPD3h8zL7SvVhQzUpump",
-      ticker: "MacCoin",
-      name: "Big Mac Coin"
-    }
-  ]
+  wallet: "CQknLDqYnrpTGaXf7uSeDEAox8b7zuwJrYDgpryKqm2u"
 };
